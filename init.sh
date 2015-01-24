928a7c736948a40bdc604ec937895c33222ee655
tsunagun <tsunagu.honma@gmail.com>
2015-01-24 21:53:09 +0900

    first commit

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudo aptitude update
+sudo aptitude safe upgrade -y
+
+sudo ufw default DENY
+sudo ufw limit ssh
+sudo ufw enable
+
+sudo sed -i -e "s/^PasswordAuthentication.*/PasswordAuthentication no/g" /etc/ssh/sshd_config
+sudo service ssh restart
