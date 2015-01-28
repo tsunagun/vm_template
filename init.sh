e4aa16e3edd4c410fc635c6dde69abb218404d7b
tsunagun <tsunagu.honma@gmail.com>
2015-01-28 19:43:35 +0900

    ufwの起動方法を修正

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ sudo aptitude safe-upgrade -y
 
 sudo ufw default DENY
 sudo ufw limit ssh
-sudo ufw enable
+yes | sudo ufw enable
 
 sudo sed -i -e "s/^PasswordAuthentication.*/PasswordAuthentication no/g" /etc/ssh/sshd_config
 sudo service ssh restart
